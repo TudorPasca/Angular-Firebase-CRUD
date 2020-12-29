2f455025b4365b06a06ca3d4fd1cb06a25df8210
Tudor Pasca <tudorpasca11@gmail.com>
2020-12-29 13:51:50 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig:
+  {
+    apiKey: "AIzaSyAIv8wuEHKGqIHyH6_tvCTrT_nlUNCu2YM",
+    authDomain: "crud-angular-firebase-dc79d.firebaseapp.com",
+    databaseURL: "https://crud-angular-firebase-dc79d-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "crud-angular-firebase-dc79d",
+    storageBucket: "crud-angular-firebase-dc79d.appspot.com",
+    messagingSenderId: "151474088126",
+    appId: "1:151474088126:web:8075534ef82251d41896ef",
+    measurementId: "G-NB10CE2HX1"
+  }
 };
